c75803639c8ce10a61050e09d551c5ae1a5b865f
Chandra Adhikari <46831935+chandraadh07@users.noreply.github.com>
2020-04-13 17:28:29 -0400

    Add files via upload

---
+++ 06/assembler.c
@@ -0,0 +1,84 @@
+//
+//  assemble.c
+//  06
+//
+//  Created by Chandra Adhikari on 4/13/20.
+//  Copyright © 2020 Chandra Adhikari. All rights reserved.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include "map.h"
+#include <string.h>
+
+
+
+//global variable,
+map compMap=NULL;
+map jumMap=NULL;
+map symbolMap=NULL;
+map despMap=NULL;
+
+int parseSymbols(char*, char*);
+int parseLine(char*, char*);
+
+int main(int argc, const char** argv) {    //./assembler add.asm add.hack
+    
+    char* inFile=argv[1];
+    char* outFile=argv[2];
+    
+    FILE * inFile=fopen(inputFile, "r"); //creating a input file in a read mode
+    FILE * inFile=fopen(inputFile, "w"); //creating a output file and write there
+
+    
+    despMap=createMap(10);
+    compMap=createMap(30); //new maps and no value inside it
+    jumMap=createMap(10);
+    symbolMap=createMap(1000);
+    
+    //first step is to go into a file, and create a symbol table of that file
+    
+    char lineRaw[200]; //assumptions that the line we read is less than 200 characters
+    //has a new line
+    
+    char lineOut[200];
+    char lineBinary[17];
+    
+    fgets(lineRaw, 200, inFile);//reads one strings from a file ;  output destination with the maximum value, and inputfile
+    
+    
+    int LineNumber=0 ;   //line number associates to that label
+    
+    while(!feof(inFile)){ //feof gives the end of end file
+        
+    //make the first pass and find all labels.
+        parseSymbols(lineRaw, LineNumber); // put the label in the symbolsdMap wih line number
+        //if C or A instruction, increment LineNumber
+        fgets(lineRaw, 200, inFile);
+        
+    } //done with the first pass;
+    //at the end of inFile
+    
+    fseek(inFile,0, SEEK_SET);
+    //now we can do the second pass
+    
+    fgets(lineRaw, 200, inFile);
+    
+    while(!feof(inFile)){
+        if(parseLine(lineRaw, lineBinary)){ //lineRaw= " M+1
+//--->101001010000// binary equivalent    }
+        fpust(lineBinary, outFile); //maybe I need to add '\0' at the end of the line??
+        fpust("\0",outFile);
+        }
+        else continue;
+        //second pass
+        
+    }
+    fclose(inFile);
+     fclose(outFile);
+    
+    
+    freeMap(compMap);
+    freeMap(jumMap);
+    freeMap(symbolMap);
+    return 0;
+}
---
+++ 06/parse.c
@@ -0,0 +1,54 @@
+//
+//  assemble.c
+//  06
+//
+//  Created by Chandra Adhikari on 4/13/20.
+//  Copyright © 2020 Chandra Adhikari. All rights reserved.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include "map.h"
+#include <string.h>
+
+#define L_COMMAND 1
+#define A_COMMAND 2
+#define C_COMMAND 3
+
+
+int parseSymbols(char*, in, int lineNumber){
+ if (commandType(in)=="L_COMMAND")   //if (commandType(in)==1)
+     return lineNumber+1;
+ else{  //command is a label
+     insertKey(symbolMap, in, lineNumber);
+     
+ }
+    
+}
+
+int parseLine(char*, char*){
+    if (commandType(in)!= L_COMMAND){
+       out= parseLCommand(in);
+        return 1;
+    }
+    else if(commandType(in)!= A_COMMAND){
+        out=parseLCommand(in);
+        return 1;
+    }
+    else if (commandType(in)!=C_COMMAND){
+        out=parseCCommand(in);
+        return 1;
+        
+    }
+    else{
+        return 0;
+    }//empty lines, comments, etc
+    
+}
+
+int commandType(char* in){
+    return 1;
+    
+    
+    
+    
+}
